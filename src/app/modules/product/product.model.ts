6d5fe22460e8a0c0778b7d03a29fdb9ef84fb29a
sajjadhussains <shuvosajjad70@gmail.com>
2024-06-17 05:51:15 +0600

    interface,model and schema done

---
+++ src/app/modules/product/product.model.ts
@@ -0,0 +1,43 @@
+import { Schema, model } from "mongoose";
+import { Inventory, Product, Variant } from "./product.interface";
+
+const variantSchema = new Schema<Variant>({
+  type: String,
+  value: String,
+});
+const inventorySchema = new Schema<Inventory>({
+  quantity: Number,
+  inStock: Boolean,
+});
+const productSchema = new Schema<Product>({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  tags: {
+    type: [String],
+    required: true,
+  },
+  variants: {
+    type: [variantSchema],
+    required: true,
+  },
+  inventory: {
+    type: inventorySchema,
+    required: true,
+  },
+});
+
+const product = model<Product>("Product", productSchema);
